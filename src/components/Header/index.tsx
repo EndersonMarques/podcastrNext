cfb2bf75d76dbe50ab05abffdfe4c427d54d0d60
EndersonMarques <emmf@ic.ufal.br>
2021-08-12 10:59:14 -0300

    sass and fonts

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,10 @@
+export function Header(){
+    return(
+        <header>
+            <img src="/logo.svg" alt="logo" />
+
+            <p>O Mlehor Para Você Sempre</p>
+            <span> Qui, 08, Abril</span>
+        </header>
+    );
+}
